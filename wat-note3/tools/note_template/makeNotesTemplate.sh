37e76f7970957a5f674c3f43d5edb6a57cfc3d77
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-02-03 23:28:07 +0900

    notes等のテンプレ作るシェルスクツール作成

---
+++ wat-note3/tools/note_template/makeNotesTemplate.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# 引数確認（第１引数：タイトル名）
+if [ $# != 1 ];
+then
+    echo "Usage: $0 title"
+    exit 1
+fi
+
+# 実行した場所のパスを記憶
+PWD_DIR=${PWD}
+
+# このシェルのあるパスへ移動
+APP_DIR=`dirname $0`
+cd ${APP_DIR}
+rc=$?
+if [ $rc != 0 ];
+then
+    echo "Error: cd"
+    cd $PWD
+    exit 1
+fi
+
+# .mdファイル作成
+filename="$1.md"
+nowDate=`date "+%Y-%m-%dT%H:%M:%S.000Z"`
+touch ${filename}
+rc=$?
+if [ $rc != 0 ];
+then
+    echo "Error: touch"
+    cd $PWD
+    exit 1
+fi
+
+# ヘッダを書き込む
+echo "---
+title: ''
+excerpt: ''
+coverImage: ''
+date: '${nowDate}'
+author:
+  name: ''
+  picture: ''
+ogImage:
+  url: ''
+---" > ${filename}
+rc=$?
+if [ $rc != 0 ];
+then
+    echo "Error: write header"
+    cd $PWD
+    exit 1
+fi
+
+# 実行元のパスへ戻る
+cd ${PWD}
+
+# 終
+exit 0
