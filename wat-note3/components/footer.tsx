e9dac52aab103e9d36ea47486eb0b3bcc72c85a9
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-02-04 15:25:14 +0900

    トップページ　記事の表示スタイル変更

--- wat-note3/components/footer.tsx
+++ wat-note3/components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <div className="py-4 flex flex-col lg:flex-row items-center">
           <h3 className="text-xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            © 2023, Built with Next.js.
           </h3>
         </div>
       </Container>
--- wat-note3/components/hero-post.tsx
+++ wat-note3/components/hero-post.tsx
@@ -1,17 +1,17 @@
-import Avatar from './avatar'
-import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
-import Link from 'next/link'
-import type Author from '../interfaces/author'
+import Avatar from "./avatar";
+import DateFormatter from "./date-formatter";
+import CoverImage from "./cover-image";
+import Link from "next/link";
+import type Author from "../interfaces/author";
 
 type Props = {
-  title: string
-  coverImage: string
-  date: string
-  excerpt: string
-  author: Author
-  slug: string
-}
+  title: string;
+  coverImage: string;
+  date: string;
+  excerpt: string;
+  author: Author;
+  slug: string;
+};
 
 const HeroPost = ({
   title,
@@ -22,11 +22,11 @@ const HeroPost = ({
   slug,
 }: Props) => {
   return (
-    <section>
+    <section className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8">
       <div className="mb-8 md:mb-16">
         <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      <div className="mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
             <Link
@@ -47,7 +47,7 @@ const HeroPost = ({
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroPost
+export default HeroPost;
