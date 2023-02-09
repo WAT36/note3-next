b1bdeb56e93559f7f64f719d1f3751980828d8ef
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-02-09 21:35:10 +0900

    Markdown->HTML変換ライブラリ変更(remark,rehype類)

--- wat-note3/lib/markdownToHtml.ts
+++ wat-note3/lib/markdownToHtml.ts
@@ -1,8 +1,16 @@
-import { remark } from "remark";
-import html from "remark-html";
+import html from "rehype-stringify";
 import gfm from "remark-gfm";
+import { unified } from "unified";
+import markdown from "remark-parse";
+import remark2rehype from "remark-rehype";
 
-export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).use(gfm).process(markdown);
+export default async function markdownToHtml(markdownContents: string) {
+  const result = await unified()
+    .use(markdown)
+    .use(remark2rehype)
+    .use(html)
+    .use(gfm)
+    .process(markdownContents);
+
   return result.toString();
 }
