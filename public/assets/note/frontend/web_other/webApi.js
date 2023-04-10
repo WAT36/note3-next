e96442a003e449420e1cbff69c9cadbf5a878c1f
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-04-10 22:09:33 +0900

    Web  APIのJS修正

--- public/assets/note/frontend/web_other/webApi.js
+++ public/assets/note/frontend/web_other/webApi.js
@@ -1,14 +1,14 @@
-var canvas = document.getElementById("sample");
-var ctx = canvas.getContext("2d");
-
-ctx.beginPath();
-ctx.rect(0, 0, 100, 100);
-ctx.strokeStyle = "#F00";
-ctx.fillStyle = "#FF0";
-ctx.stroke();
-ctx.fill();
-
 window.onload = function () {
+  var canvas = document.getElementById("sample");
+  var ctx = canvas.getContext("2d");
+
+  ctx.beginPath();
+  ctx.rect(0, 0, 100, 100);
+  ctx.strokeStyle = "#F00";
+  ctx.fillStyle = "#FF0";
+  ctx.stroke();
+  ctx.fill();
+
   var title = document.getElementById("title");
   var title_nonstorage = document.getElementById("title_nonstorage");
   var boxes = document.getElementsByClassName("box");
