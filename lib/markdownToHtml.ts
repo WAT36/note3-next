01246596c6e4097b5f00ed18251c9b495ad285f1
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-12 00:00:22 +0900

    mmarkdownからのパース時にKaTeXの変換処理を組み込み

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -4,12 +4,17 @@ import remark2rehype from "remark-rehype";
 import remarkParse from "remark-parse";
 import rehypeStringify from "rehype-stringify";
 
+import math from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 export default async function markdownToHtml(markdownContents: string) {
   const result = await unified()
     .use(remarkParse) // markdown -> mdast の変換
+    .use(math)
     .use(remark2rehype, { allowDangerousHtml: true }) // mdast -> hast の変換
     .use(rehypeStringify, { allowDangerousHtml: true }) // hast -> html の変換
     .use(gfm)
+    .use(rehypeKatex)
     .process(markdownContents);
 
   return result.toString();
