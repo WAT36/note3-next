625dc1622dd887dbf75005f22ef6d66e47e561fc
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-02-05 00:05:03 +0900

    Noteページ（記事用ページ）のコンポーネント 作成

---
+++ components/note-page.tsx
@@ -0,0 +1,45 @@
+import Layout from "./layout";
+import Container from "./container";
+import Head from "next/head";
+import NoteType from "../interfaces/note";
+import { TITLE } from "../lib/constants";
+import PostHeader from "./post-header";
+import PostBody from "./post-body";
+
+type Props = {
+  note: NoteType;
+};
+
+const NotePage = ({ note }: Props) => {
+  return (
+    <Layout programmingTag={note.programming}>
+      <Container>
+        <article className="mb-32">
+          <Head>
+            <title>
+              {note.title} | {TITLE}
+            </title>
+            {note.ogImage ? (
+              <meta property="og:image" content={note.ogImage.url} />
+            ) : (
+              <></>
+            )}
+            {note.link?.css &&
+              note.link.css.map((cssPath) => {
+                return <link rel="stylesheet" type="text/css" href={cssPath} />;
+              })}
+          </Head>
+          <PostHeader
+            title={note.title}
+            coverImage={note.coverImage}
+            date={note.date}
+            author={note.author}
+          />
+          <PostBody content={note.content} />
+        </article>
+      </Container>
+    </Layout>
+  );
+};
+
+export default NotePage;
