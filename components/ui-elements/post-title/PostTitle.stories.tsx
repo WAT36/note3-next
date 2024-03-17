d0bd3d387efad0d4c9c4d5c15fa227c88f266cd7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-17 16:23:32 +0900

    storiesフォルダ調整

--- components/ui-elements/post-title/PostTitle.stories.tsx
+++ components/ui-elements/post-title/PostTitle.stories.tsx
@@ -4,7 +4,7 @@ import PostTitle from "./PostTitle";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: "Molecules/PostTitle",
+  title: "Atom/PostTitle",
   component: PostTitle,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
