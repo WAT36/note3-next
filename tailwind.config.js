85fc60ed0bccd9ca7fada926d82abb31a7791bd8
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-18 20:04:19 +0900

    tailwind設定修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  content: ["./components/**/*.{ts,tsx}", "./pages/**/*.tsx"],
   theme: {
     extend: {
       colors: {
