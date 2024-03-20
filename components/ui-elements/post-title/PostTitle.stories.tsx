9f6fd574835099406441763aa1c8816186065229
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-20 12:35:21 +0900

    PostTitle

--- components/ui-elements/post-title/PostTitle.stories.tsx
+++ components/ui-elements/post-title/PostTitle.stories.tsx
@@ -20,6 +20,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Main: Story = {
   args: {
-    children: <></>,
+    children: <p>{"記事タイトルテスト"}</p>,
   },
 };
