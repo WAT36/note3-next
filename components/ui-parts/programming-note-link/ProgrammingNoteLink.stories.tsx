d08d0ad3336585c2840ef53731950e69e47966a6
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-30 17:21:22 +0900

    ProgramminngNoteLink story追加

--- components/ui-parts/programming-note-link/ProgrammingNoteLink.stories.tsx
+++ components/ui-parts/programming-note-link/ProgrammingNoteLink.stories.tsx
@@ -1,11 +1,13 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import ProgrammingNoteLink from "./ProgrammingNoteLink";
+import { RecoilRoot } from "recoil";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: "Molecules/ProgrammingNoteLink",
   component: ProgrammingNoteLink,
+  decorators: [(story) => <RecoilRoot>{story()} </RecoilRoot>],
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: "centered",
