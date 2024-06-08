2ee42cc0b8e13b373b7d0b8ad51372eaffbb32e1
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-06-08 17:29:59 +0900

    GAタグ、マネージャー対応

--- components/ui-pages/layout/Layout.tsx
+++ components/ui-pages/layout/Layout.tsx
@@ -2,6 +2,8 @@ import Footer from "../../ui-elements/footer/Footer";
 import Header from "../../ui-parts/header/Header";
 import { Meta } from "../../ui-elements/meta/Meta";
 import SubHeader from "../../ui-parts/subheader/SubHeader";
+import { CSSProperties } from "react";
+import { GTAG_ID } from "../../../lib/gtag";
 
 type Props = {
   preview?: boolean;
@@ -9,10 +11,32 @@ type Props = {
   canChangeProgrammingLanguage?: boolean;
 };
 
+const gtagNoscriptStyle: CSSProperties = {
+  display: "none",
+  visibility: "hidden",
+};
+const gtagUrl = "https://www.googletagmanager.com/ns.html?id=" + GTAG_ID;
+
 const Layout = ({ preview, children, canChangeProgrammingLanguage }: Props) => {
   return (
     <>
       <Meta />
+      {process.env.NEXT_PUBLIC_APP_ENV === "prd" ? (
+        <>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe
+              src={gtagUrl}
+              height="0"
+              width="0"
+              style={gtagNoscriptStyle}
+            ></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
+        </>
+      ) : (
+        <></>
+      )}
       <Header />
       <div className="min-h-screen">
         <SubHeader
