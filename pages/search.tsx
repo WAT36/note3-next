d7dec57e0e3332d42b7f3acec047cc481c9276d4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-08-24 17:28:12 +0900

    検索結果のデザイン少し変えた

--- pages/search.tsx
+++ pages/search.tsx
@@ -14,14 +14,15 @@ const searchClient = algoliasearch(
 
 function Hit({ hit }) {
   return (
-    <>
+    <div className="my-4">
       <Link
-        className="font-bold text-xl my-5 text-blue-900"
+        className="font-bold text-xl m-4 text-blue-900"
         href={hit.path + process.env.NEXT_PUBLIC_URL_END}
       >
         {hit.title}
       </Link>
-    </>
+      <p className="m-4 w-full">{hit.excerpt || "　"}</p>
+    </div>
   );
 }
 
@@ -35,6 +36,7 @@ export default function SearchResult() {
           <Intro title={"記事検索"} />
           <Bio admin={ADMINISTRATOR} />
           {/**TODO 初期表示時に初期検索が行われないようにする */}
+          {/**TODO 検索結果0軒の時の表示どうすれば良いのか？ */}
           <InstantSearch searchClient={searchClient} indexName={indexName}>
             <Configure hitsPerPage={5} />
             <SearchBox
