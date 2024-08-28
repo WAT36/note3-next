4591faa149e818a76120aca8ed83d06c10c8c0ec
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-08-28 22:01:39 +0900

    とりあえずタグリスト取得して列挙させるまではできた　このタグから該当の記事持って来させるとこを次やる

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,12 +5,21 @@ import Intro from "../components/ui-elements/intro/Intro";
 import Layout from "../components/ui-pages/layout/Layout";
 import { ADMINISTRATOR } from "../lib/constants";
 import algoliasearch from "algoliasearch/lite";
-import { Configure, Hits, InstantSearch, SearchBox } from "react-instantsearch";
+import {
+  Configure,
+  Hits,
+  Index,
+  InstantSearch,
+  SearchBox,
+} from "react-instantsearch";
+import { useEffect, useState } from "react";
 
 const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_APP_ID || "",
   process.env.NEXT_PUBLIC_SEARCH_DATA_API_KEY || ""
 );
+const indexName = process.env.NEXT_PUBLIC_INDEX_NAME || "";
+const index = searchClient.initIndex(indexName);
 
 function Hit({ hit }) {
   return (
@@ -27,7 +36,16 @@ function Hit({ hit }) {
 }
 
 export default function SearchResult() {
-  const indexName = process.env.NEXT_PUBLIC_INDEX_NAME || "";
+  const [tagList, setTagList] = useState<string[]>([]);
+  useEffect(() => {
+    index.searchForFacetValues("_tags", "").then(({ facetHits }) => {
+      setTagList(
+        facetHits.map((hit) => {
+          return hit.value;
+        })
+      );
+    });
+  }, []);
 
   return (
     <>
@@ -38,6 +56,7 @@ export default function SearchResult() {
           {/**TODO 初期表示時に初期検索が行われないようにする */}
           {/**TODO 検索結果0軒の時の表示どうすれば良いのか？ */}
           <InstantSearch searchClient={searchClient} indexName={indexName}>
+            <Index indexName={indexName}>{/* Widgets */}</Index>
             <Configure hitsPerPage={5} />
             <SearchBox
               placeholder={"検索"}
@@ -48,6 +67,14 @@ export default function SearchResult() {
               }}
               resetIconComponent={({ classNames }) => <></>}
             />
+            <div className="w-full flex flex-wrap">
+              <span className="py-4">{"タグ："}</span>
+              {tagList.length > 0
+                ? tagList.map((tag) => {
+                    return <span className="p-4 border-blue-500">{tag}</span>;
+                  })
+                : ""}
+            </div>
             <Hits hitComponent={Hit} />
           </InstantSearch>
         </Container>
