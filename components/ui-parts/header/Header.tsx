5c3a69b6995fc8631ad37a993affa43c0db3dab6
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-01-21 00:47:14 +0900

    ヘッダーのリンク集をレスポンシブ対応化　幅小さい時はフッターに表示させるようにした

--- components/ui-parts/header/Header.tsx
+++ components/ui-parts/header/Header.tsx
@@ -1,20 +1,8 @@
 import Link from "next/link";
 import { TITLE } from "../../../lib/constants";
-import { useRouter } from "next/router";
+import BarLinkGroup from "../../ui-elements/barLinkGroup/BarLinkGroup";
 
 const Header = () => {
-  const router = useRouter();
-  const searchButtonClick = () => {
-    const element = document.getElementById(
-      "search-textbox"
-    ) as HTMLInputElement;
-
-    router.push({
-      pathname: "/search",
-      query: { query: element.value },
-    });
-  };
-
   return (
     <header className="bg-gray-400 text-white z-50 dark:bg-slate-900">
       <Link
@@ -24,37 +12,8 @@ const Header = () => {
         {TITLE}
       </Link>
 
-      <span className="float-right">
-        <Link
-          className="font-bold no-underline text-xl mx-3 text-white"
-          href={"/search" + process.env.NEXT_PUBLIC_URL_END}
-        >
-          Search
-        </Link>
-        <Link
-          className="font-bold no-underline text-xl mx-3 text-white"
-          href={"/posts" + process.env.NEXT_PUBLIC_URL_END}
-        >
-          Blog
-        </Link>
-        <Link
-          className="font-bold no-underline text-xl mx-3 text-white"
-          href={"/notes" + process.env.NEXT_PUBLIC_URL_END}
-        >
-          Notes
-        </Link>
-        <Link
-          className="font-bold no-underline text-xl mx-3 text-white"
-          href={"/about" + process.env.NEXT_PUBLIC_URL_END}
-        >
-          About
-        </Link>
-        <Link
-          className="font-bold no-underline text-xl mx-3 text-white"
-          href={"/disclaimer" + process.env.NEXT_PUBLIC_URL_END}
-        >
-          免責事項
-        </Link>
+      <span className="float-right hidden lg:block">
+        <BarLinkGroup />
       </span>
     </header>
   );
