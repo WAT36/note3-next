66a94461fc2c7ce610d14b38ca54342b08df5d2a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-01-28 13:37:12 +0900

    パンくず　クエリパラメータ類の時も出さないようにさせる

--- components/ui-elements/bread-crumb/BreadCrumb.tsx
+++ components/ui-elements/bread-crumb/BreadCrumb.tsx
@@ -8,8 +8,13 @@ export const BreadCrumb: NextPage = () => {
 
   // pathを「/」で分解
   const paths = decodeURI(router.asPath).substring(1).split("/");
-  // 末尾がindex.htmlだった場合それは削除
-  if (paths.slice(-1)[0] === "index.html" || paths.slice(-1)[0] === "") {
+  // 末尾がindex.html・クエリパラメータ類だった場合それは削除
+  if (
+    paths.slice(-1)[0] === "index.html" ||
+    paths.slice(-1)[0] === "" ||
+    paths.slice(-1)[0][0] === "#" ||
+    paths.slice(-1)[0][0] === "?"
+  ) {
     paths.pop();
   }
 
