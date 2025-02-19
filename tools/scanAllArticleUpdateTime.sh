a2817b58a1c85badf60023d9700f95627ae08357
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-02-19 21:49:50 +0900

    前回プッシュ時から変更のあったファイルのupdateAtを更新するバッチを作成

--- tools/scanAllArticleUpdateTime.sh
+++ tools/scanAllArticleUpdateTime.sh
@@ -21,22 +21,30 @@ PWD_DIR=${PWD}
 APP_DIR=$(dirname $0)
 cd ${APP_DIR}
 
-# まず_notesへ移動
-cd ../_notes
+# ルートディレクトリへ移動
+cd ../
 
-# 次に_postsへ移動
-cd ../_posts
-
-# 現在のディレクトリ以下の全ての `.md` ファイルを検索して処理
-find . -type f -name "*.md" | while read -r file; do
+# 前回のgit pushから変更のあった全ての `.md` ファイルを検索して処理
+count=0
+git log --diff-filter=ACMRT --name-status --pretty=format: $(git rev-parse @{push})..HEAD | awk '$1 != "D" {print $NF}' | grep -E '.md$' | sort | uniq | while read -r file; do
     # 更新時刻を取得（フォーマット: YYYY-MM-DD HH:MM:SS）
     mod_time=$(get_mod_time "$file")
 
-    # ファイルの末尾に追加
-    echo -e "\n--- Updated at: $mod_time --- $file"
-    # echo -e "\n--- Updated at: $mod_time ---" >> "$file"
+    # ファイルからupdatedAtを取得
+    file_time=$(grep 'updatedAt:' "$file" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.000Z')
 
-    # echo "Updated timestamp added to: $file"
+    # ファイルの末尾に追加
+    if [[ "$file_time" = "" ]]; then
+        # たまにあるが updatedAtないファイルは更新しない
+        echo -e "updatedAt none. $file"
+    elif [[ "$mod_time" != "$file_time" ]]; then
+        sed -i '' "s/updatedAt:.*/updatedAt: '${mod_time}'/g" $file
+        ((count++))
+    fi
 done
+
+# Git に追加 & コミット & プッシュ
+git add .
+git commit -m "Update updatedAt: ${count} files."
 
 cd $PWD
