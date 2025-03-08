b99f1a5f27bccf8b08bd996853865cc470df23d1
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-03-09 02:17:26 +0900

    更新した日にちが違うかでupdatedAt更新するか判断するように修正

--- tools/scanAllArticleUpdateTime.sh
+++ tools/scanAllArticleUpdateTime.sh
@@ -34,11 +34,15 @@ git log --diff-filter=A --name-status --pretty=format: $(git rev-parse @{push}).
     # ファイルからupdatedAtを取得
     file_time=$(grep 'updatedAt:' "$file" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.000Z')
 
+    # YYYY-MM-DD の部分だけを抽出
+    mod_day=$(echo "$mod_time" | cut -d ' ' -f 1)
+    file_day=$(echo "$file_time" | cut -d ' ' -f 1)
+
     # .mdファイルの更新時刻更新
     if [[ "$file_time" = "" ]]; then
         # たまにあるが updatedAtないファイルは更新しない
         echo -e "updatedAt none. $file"
-    elif [[ "$mod_time" != "$file_time" ]]; then
+    elif [[ "$mod_day" != "$file_day" ]]; then
         # 作成時刻、更新時刻を更新
         sed -i '' "s/date:.*/date: '${mod_time}'/g" $file
         sed -i '' "s/updatedAt:.*/updatedAt: '${mod_time}'/g" $file
@@ -54,11 +58,15 @@ git log --diff-filter=CMRT --name-status --pretty=format: $(git rev-parse @{push
     # ファイルからupdatedAtを取得
     file_time=$(grep 'updatedAt:' "$file" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.000Z')
 
+    # YYYY-MM-DD の部分だけを抽出
+    mod_day=$(echo "$mod_time" | cut -d ' ' -f 1)
+    file_day=$(echo "$file_time" | cut -d ' ' -f 1)
+
     # .mdファイルの更新時刻更新
     if [[ "$file_time" = "" ]]; then
         # たまにあるが updatedAtないファイルは更新しない
         echo -e "updatedAt none. $file"
-    elif [[ "$mod_time" != "$file_time" ]]; then
+    elif [[ "$mod_day" != "$file_day" ]]; then
         sed -i '' "s/updatedAt:.*/updatedAt: '${mod_time}'/g" $file
         ((count++))
     fi
@@ -66,6 +74,6 @@ done
 
 # Git に追加 & コミット & プッシュ
 git add .
-git commit -m "Update updatedAt: ${count} files."
+git commit -m "Update updatedAt: $count files."
 
 cd $PWD
