56ea333f9ff809f48145533fbf36d51391e01dbe
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-03-12 00:18:17 +0900

    rss作成ロジック修正

--- src/lib/generateRSS.ts
+++ src/lib/generateRSS.ts
@@ -2,7 +2,8 @@ import fs from "fs";
 import { Feed } from "feed";
 import { ADMINISTRATOR, DESCRIPTION, TITLE } from "./constants";
 import * as dotenv from "dotenv";
-import { getAllArticle } from "./algolia";
+import { fileURLToPath } from "url";
+import path from "path";
 
 dotenv.config();
 
@@ -19,42 +20,126 @@ interface Post {
   description: string;
 }
 
+// mdファイルリストを読み込む
+const listFiles = (dir) =>
+  fs
+    .readdirSync(dir, { withFileTypes: true })
+    .flatMap((dirent) =>
+      dirent.isFile()
+        ? dirent.name.endsWith(".md")
+          ? [`${dir}/${dirent.name}`]
+          : []
+        : listFiles(`${dir}/${dirent.name}`)
+    );
+
+// 現在のカレントディレクトリを記憶
+const originalDir = process.cwd();
+
 export async function generateRSSFeed() {
-  // TODO any無くしたい
-  const posts: Post[] = (await getAllArticle()).map((hit: any) => {
-    return {
-      title: hit.title,
-      link: hit.path + process.env.NEXT_PUBLIC_URL_END,
-      date: new Date(hit.date),
-      description: hit.excerpt,
-    };
-  });
+  try {
+    // このファイルがあるディレクトリのパスを取得
+    const __filename = fileURLToPath(import.meta.url);
+    const scriptDir = path.dirname(__filename);
 
-  const feed = new Feed({
-    title: blogTitle,
-    description: blogDescription,
-    id: siteUrl,
-    link: siteUrl,
-    language: "ja",
-    updated: new Date(),
-    copyright: ADMINISTRATOR.nickname,
-  });
+    // スクリプトのディレクトリに移動
+    process.chdir(scriptDir);
 
-  // 記事をフィードに追加
-  posts.forEach((post) => {
-    feed.addItem({
-      title: post.title,
-      id: siteUrl + post.link,
-      link: siteUrl + post.link,
-      date: post.date,
-      description: post.description,
+    let files = [];
+    files = files.concat(listFiles("../../_posts"));
+
+    const feed = new Feed({
+      title: blogTitle,
+      description: blogDescription,
+      id: siteUrl,
+      link: siteUrl,
+      language: "ja",
+      updated: new Date(),
+      copyright: ADMINISTRATOR.nickname,
     });
-  });
 
-  // `public/` ディレクトリに RSS フィードを書き出す
-  fs.writeFileSync("public/rss.xml", feed.rss2()); // RSS 2.0 形式
+    // for文で１個１個ファイル名を取っていく
+    for (let i = 0; i < files.length; i++) {
+      let filepath = files[i];
+      // それをもとにファイル読み込み
+      if (!fs.existsSync(filepath)) {
+        console.log(`${filepath}は存在しません`);
+        continue;
+      }
+      const data = fs.readFileSync(filepath, "utf8");
 
-  console.log("✅ RSSフィードを生成しました！");
+      // .mdの冒頭のタイトルとかを読み込み
+      // それで１個１個jsonレコード作る
+      // TODO algoliaとほぼ同じなので　統一するか共通化したい
+
+      // タイトル抜き出し
+      let extractedData = data.match(/title:.*\n/);
+      const title =
+        extractedData && extractedData.length > 0
+          ? extractedData[0]
+              .replace("title:", "")
+              .replaceAll(/(\"|\')/g, "")
+              .trim()
+          : "";
+
+      // 概要抜き出し
+      extractedData = data.match(/excerpt:.*\n/);
+      const excerpt =
+        extractedData && extractedData.length > 0
+          ? extractedData[0]
+              .replace("excerpt:", "")
+              .replaceAll(/(\"|\')/g, "")
+              .trim()
+          : "";
+
+      // 日時抜き出し
+      extractedData = data.match(/date:.*\n/);
+      const date =
+        extractedData && extractedData.length > 0
+          ? extractedData[0]
+              .replace("date:", "")
+              .replaceAll(/(\"|\')/g, "")
+              .trim()
+          : undefined;
+
+      // 画像ファイル名抜き出し
+      extractedData = data.match(/coverImage:.*\n/);
+      const coverImage =
+        extractedData && extractedData.length > 0
+          ? extractedData[0]
+              .replace("coverImage:", "")
+              .replaceAll(/(\"|\')/g, "")
+              .trim()
+          : undefined;
+
+      // ファイルパス　レコード用に書き換え
+      const fileSlug = filepath
+        .replace("../../_notes", "/notes")
+        .replace("../../_posts", "/posts");
+
+      // _index.md の場合は登録しない(スキップ)
+      if (fileSlug.endsWith("_index.md")) {
+        continue;
+      }
+
+      feed.addItem({
+        title,
+        id: siteUrl + fileSlug.replace(".md", ""),
+        link: siteUrl + fileSlug.replace(".md", ""),
+        date: new Date(date),
+        description: excerpt,
+      });
+    }
+
+    // `public/` ディレクトリに RSS フィードを書き出す
+    fs.writeFileSync("../../out/rss.xml", feed.rss2()); // RSS 2.0 形式
+
+    console.log("✅ RSSフィードを生成しました！");
+  } catch (error) {
+    console.error("エラーが発生しました ❌:", error);
+  } finally {
+    // 元のディレクトリに戻る
+    process.chdir(originalDir);
+  }
 }
 
 generateRSSFeed().catch((err) => {
