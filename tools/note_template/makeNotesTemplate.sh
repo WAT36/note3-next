2548bf70732905be302a87fb30f467c92e88b186
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-05-18 01:58:38 +0900

    ファイル作成バッチ　オプション指定時に記事フォルダに移動させる

--- tools/note_template/makeNotesTemplate.sh
+++ tools/note_template/makeNotesTemplate.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
 # 引数確認（第１引数：タイトル名）
-if [ $# != 1 ];
-then
-    echo "Usage: $0 title"
-    exit 1
+if [[ "$#" -lt 1 ]]; then
+  echo "Usage: $0 title [--post]"
+  exit 1
 fi
+
+filename=$1
+
+# オプション確認、--postがあった場合は_postsへ移動する
+post_option=false
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+  --post)
+    post_option=true
+    ;;
+  *) ;;
+  esac
+  shift
+done
 
 # 実行した場所のパスを記憶
 PWD_DIR=${PWD}
 
 # このシェルのあるパスへ移動
-APP_DIR=`dirname $0`
+APP_DIR=$(dirname $0)
 cd ${APP_DIR}
 rc=$?
-if [ $rc != 0 ];
-then
-    echo "Error: cd"
-    cd $PWD
-    exit 1
+if [ $rc != 0 ]; then
+  echo "Error: cd"
+  cd $PWD
+  exit 1
 fi
 
 # .mdファイル作成
-filename="$1.md"
-nowDate=`date "+%Y-%m-%dT%H:%M:%S.000Z"`
+filename="${filename}.md"
+nowDate=$(date "+%Y-%m-%dT%H:%M:%S.000Z")
 touch ${filename}
 rc=$?
-if [ $rc != 0 ];
-then
-    echo "Error: touch"
-    cd $PWD
-    exit 1
+if [ $rc != 0 ]; then
+  echo "Error: touch"
+  cd $PWD
+  exit 1
 fi
 
 # ヘッダを書き込む
@@ -46,13 +57,18 @@ author:
   picture: '/assets/blog/authors/WAT.jpg'
 ogImage:
   url: ''
----" > ${filename}
+---" >${filename}
 rc=$?
-if [ $rc != 0 ];
-then
-    echo "Error: write header"
-    cd $PWD
-    exit 1
+if [ $rc != 0 ]; then
+  echo "Error: write header"
+  cd $PWD
+  exit 1
+fi
+
+# --postオプションある場合は/_postsへ移動
+if $post_option; then
+  mv ${filename} ../../_posts
+  echo "/_posts に移動しました"
 fi
 
 # 実行元のパスへ戻る
