3946cd518d6087f3fba68c34d8d584e9435c5930
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-06-08 18:06:56 +0900

    テストを開発環境に置き換え・Login UIを突破するようにスクリプト変更

--- tests/constant.ts
+++ tests/constant.ts
@@ -2,3 +2,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const TOP_PAGE_URL = process.env.PLAYWRIGHT_TEST_ROOTPAGE;
+
+export const LOGIN_USERNAME = process.env.PLAYWRIGHT_TEST_USERNAME;
+export const LOGIN_PASSWORD = process.env.PLAYWRIGHT_TEST_PASSWORD;
--- tests/visual/top.spec.ts
+++ tests/visual/top.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { TOP_PAGE_URL } from "../constant";
+import { doLogin } from "../login";
 
 test("top page test", async ({ page }) => {
-  await page.goto(TOP_PAGE_URL);
+  await doLogin({ page, pageUrl: TOP_PAGE_URL });
   await expect(page).toHaveScreenshot({
     fullPage: true,
     maxDiffPixels: 100,
