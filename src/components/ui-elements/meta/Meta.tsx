9641051a7191729ec71ef180f1039c647efcb162
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-09-30 00:49:22 +0900

    JSXGraphやっと表示できた・・・Cursor恐るべし

--- src/components/ui-elements/meta/Meta.tsx
+++ src/components/ui-elements/meta/Meta.tsx
@@ -5,13 +5,14 @@ import { GA_ID, GTAG_ID } from "../../../lib/gtag";
 
 export const Meta = () => {
   return (
-    <Head>
-      {process.env.NEXT_PUBLIC_APP_ENV === "prd" ? (
-        <>
-          {/* <!-- Google Tag Manager --> */}
-          <Script
-            dangerouslySetInnerHTML={{
-              __html: `
+    <>
+      <Head>
+        {process.env.NEXT_PUBLIC_APP_ENV === "prd" ? (
+          <>
+            {/* <!-- Google Tag Manager --> */}
+            <Script
+              dangerouslySetInnerHTML={{
+                __html: `
             (function (w, d, s, l, i) {
               w[l] = w[l] || [];
               w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
@@ -22,59 +23,67 @@ export const Meta = () => {
               j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
               f.parentNode.insertBefore(j, f);
             })(window, document, "script", "dataLayer", "${GTAG_ID}");`,
-            }}
-          />
-          {/* <!-- End Google Tag Manager --> */}
-        </>
-      ) : (
-        <></>
-      )}
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+              }}
+            />
+            {/* <!-- End Google Tag Manager --> */}
+          </>
+        ) : (
+          <></>
+        )}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+          integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraph.css"
+        />
+        <meta
+          name="description"
+          content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        />
+        <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      </Head>
+      {/* Load JSXGraph core before any inline scripts in content execute */}
+      <Script
+        src="https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraphcore.js"
+        strategy="beforeInteractive"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
-        integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
-        crossOrigin="anonymous"
-      />
-      <Script src="https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraphcore.js"></Script>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraph.css"
-      />
-      <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
-    </Head>
+    </>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import { AppProps } from "next/app";
 import "highlight.js/styles/github-dark.css";
 import "../styles/index.css";
 import { RecoilRoot, RecoilEnv } from "recoil";
+import Script from "next/script";
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraphcore.js"
+        strategy="beforeInteractive"
+      />
       <Component {...pageProps} />
     </RecoilRoot>
   );
